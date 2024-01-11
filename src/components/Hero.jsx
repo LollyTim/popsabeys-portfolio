0180ed2cbae6510db773a41ac6890e3d27620a1c
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-11 14:14:09 +0100

    just started the hero sectiopn here

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
-const Home = () => {
+const Hero = () => {
   return (
-    <div className=''>
-        
+    <div className='w-[48%] justify-center items-center mx-auto mt-24 mb-16 border'>
+      <p className=' flex py-2 px-5 border border-solid border-turquoise-300 border-opacity-44'>I am eager to hear what you’re working on</p>
+        {/* wjgfhjwfgjhwvjf<br />
+        wjgfhjwfgjhwvjfwfbw<br />
+        wkfw<br />
+        wfbwjf<br />
+        wbfw
+        w <br />
+        jffbfwjfjwbfwjbf <br />
+        vjnedvbjebjdsbvjdbvjdbdkj  */}
     </div>
   )
 }
 
-export default Home
+export default Hero
