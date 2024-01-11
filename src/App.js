41e81d54e77ff22a14399c108cfea9b9125bfe2a
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-11 13:23:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div className="">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
