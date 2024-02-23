8c6c7b7759a0d70a6aec1ef5ce470c10170405c5
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-02-23 09:10:02 +0100

    updated the nav header

--- src/components/WhatIdo.jsx
+++ src/components/WhatIdo.jsx
@@ -15,11 +15,11 @@ const WhatIdo = () => {
             ease: "easeIn",
             duration: 1,
           }}
-          className="gradient2 font-SatoshiBold font-bold bg-clip-text text-[30px] lg:text-[40px]"
+          className="gradient2 font-SatoshiBold font-bold bg-clip-text text-[22px]  lg:text-[40px] text-center justify-center items-center"
         >
-          What I do
+          I help businesses improve via
         </motion.h1>
-        <motion.p
+        {/* <motion.p
           initial={{ x: -100, opacity: 0 }}
           whileInView={{ x: 0, opacity: 1 }}
           viewport={{ once: true }}
@@ -34,9 +34,9 @@ const WhatIdo = () => {
         >
           From Product Design to Identity Design to Pitch Deck Design etc., your
           project is in great hands
-        </motion.p>
+        </motion.p> */}
       </div>
-      <div className="w-[90%] lg:w-full justify-center lg:justify-start grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-5 lg:items-start items-center">
+      <div className="w-[90%] lg:w-full justify-center lg:justify-start grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-5 lg:items-start items-center text-center">
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
           whileInView={{ opacity: 1, scale: 1 }}
@@ -97,6 +97,25 @@ const WhatIdo = () => {
             Pitch Deck Design
           </p>
         </motion.div>
+
+        {/* <motion.div
+          initial={{ opacity: 0.1, scale: 0 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          viewport={{ once: true }}
+          transition={{
+            duration: 0.5,
+            delay: 0.8,
+            x: { type: "inertia", bounceStiffness: 30 },
+          }}
+          className=" sm:py-7 sm:px-10 py-3 px-3 justify-center w-[100%] lg:max-w-[268px] border rounded-lg border-[#282B46] text-center items-center"
+        >
+          <p className=" font-SatoshiLight font-normal text-white text-sm xl:text-xl text-nowrap">
+            UX Research
+          </p>
+        </motion.div> */}
+      </div>
+      {/* second line here */}
+      <div className="w-[90%] lg:w-[80%] justify-center lg:justify-center mt-6 grid grid-cols-1 sm:grid-cols-3 lg:flex xl:flex gap-5 lg:items-center items-center text-center lg:mx-auto">
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
           viewport={{ once: true }}
@@ -106,7 +125,7 @@ const WhatIdo = () => {
             delay: 0.5,
             x: { type: "inertia", bounceStiffness: 30 },
           }}
-          className=" sm:py-7 sm:px-10 py-3 px-3 justify-center w-[100%] lg:max-w-[268px] border rounded-lg border-[#282B46] text-center  items-center"
+          className=" sm:py-7 sm:px-10 py-3 px-3 justify-center w-[100%] lg:max-w-[268px]  border rounded-lg border-[#282B46] text-center  items-center"
         >
           <p className=" font-SatoshiLight font-normal text-white text-sm xl:text-xl text-nowrap">
             Project Management
@@ -140,21 +159,6 @@ const WhatIdo = () => {
         >
           <p className=" font-SatoshiLight font-normal text-white text-sm xl:text-xl text-nowrap">
             Web Development
-          </p>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0.1, scale: 0 }}
-          whileInView={{ opacity: 1, scale: 1 }}
-          viewport={{ once: true }}
-          transition={{
-            duration: 0.5,
-            delay: 0.8,
-            x: { type: "inertia", bounceStiffness: 30 },
-          }}
-          className=" sm:py-7 sm:px-10 py-3 px-3 justify-center w-[100%] lg:max-w-[268px] border rounded-lg border-[#282B46] text-center items-center"
-        >
-          <p className=" font-SatoshiLight font-normal text-white text-sm xl:text-xl text-nowrap">
-            UX Research
           </p>
         </motion.div>
       </div>
