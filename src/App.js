70458703c228ce67d1f3e2fc3132b78c5b00dcf2
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-02-26 23:39:22 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -110,7 +110,7 @@ function App() {
         style={{ zindex: 100 }}
         className="overflow-hidden"
       >
-        <FaArrowCircleUp className="text-4xl text-slate-40 rotate-[-1deg] overflow-hidden animate-pulse duration-[22ms]" />
+        <FaArrowCircleUp className="text-4xl text-slate-40 rotate-[-1deg] overflow-hidden" />
       </ScrollToTop>
     </div>
   );
