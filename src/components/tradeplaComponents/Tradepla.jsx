de2eae556a57405ed371c8ab65e9537f761f0ebf
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-03-12 17:32:46 +0100

    updated

--- src/components/tradeplaComponents/Tradepla.jsx
+++ src/components/tradeplaComponents/Tradepla.jsx
@@ -48,7 +48,7 @@ const Tradepla = () => {
           ease: "easeIn",
           duration: 1,
         }}
-        className=" overflow-hidden flex flex-col justify-between  mt-[96px] max-[768px]:max-w-[360px] lg:max-w-[1050px] lg:h-[260px] h-[180px] bg-[#0F1123] rounded-lg mx-auto px-14 pt-[18px] max-[768px]:px-7 pb-8"
+        className=" overflow-hidden flex flex-col justify-between  mt-[96px] max-[768px]:max-w-[360px] lg:max-w-[85%] lg:h-[260px] h-[180px] bg-[#0F1123] rounded-lg mx-auto px-14 pt-[18px] max-[768px]:px-7 pb-8"
       >
         <p className=" font-SatoshiLight text-base font-normal ">
           Summarized project scope
@@ -236,7 +236,7 @@ const Tradepla = () => {
                 duration: 1,
               }}
               key={index}
-              className="w-[340px] max-[390px]:w-[320px] grid h-[400px] lg:col-span-1 lg:w-full lg:h-[700px] rounded-xl hover:transition"
+              className="w-[340px] max-[390px]:w-[320px] grid h-[350px] lg:col-span-1 lg:w-full lg:h-[600px] rounded-xl hover:transition"
             >
               <div
                 className="w-full h-full rounded-xl"
