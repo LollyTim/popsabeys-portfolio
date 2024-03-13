821a800d6b24a7386cafb680041b1a4d6da92f1b
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-03-13 23:58:04 +0100

    updated

--- src/components/proattireComponents/Section1.jsx
+++ src/components/proattireComponents/Section1.jsx
@@ -41,6 +41,8 @@ const Section1 = () => {
         </div>
       </motion.div>
 
+      {/* another section starts here  */}
+
       <motion.div
         initial={{ x: -200, opacity: 0 }}
         whileInView={{ x: 0, opacity: 1 }}
