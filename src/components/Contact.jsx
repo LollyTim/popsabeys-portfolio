088184d2c280af2e84a50c351112a9c75b9ad278
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-03-19 00:07:46 +0100

    updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { TextField } from "@mui/material";
 import { act } from "react-dom/test-utils";
+import InputComponents from "./InputComponents";
 
 const Contact = () => {
   return (
-    <div className=" w-full h-[90vh] flex justify-center items-center">
-      <div className="h-[80%] mx-auto w-[50%] border justify-center items-center">
-        <h1 className=" justify-center flex items-center">
+    <div className=" w-full flex justify-center items-center ">
+      <div className=" mx-auto lg:w-[50%] w-[90%] rounded-xl bg-[#0F1123] px-10 py-10 gap-10 flex flex-col">
+        <h1 className=" justify-center text-[24px] flex items-center">
           Tell me a little about your brand/idea
         </h1>
-        <form className="jutstify-center flex items-center"></form>
+        <form className="jutstify-center flex items-center w-full flex-col gap-3">
+          <InputComponents title={"Your name*"} />
+          <InputComponents title={"Your name*"} />
+          <InputComponents title={"Your name*"} />
+          <InputComponents title={"Your name*"} />
+        </form>
       </div>
     </div>
   );
---
+++ src/components/InputComponents.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const InputComponents = ({ title }) => {
+  return (
+    <div className="w-full relative text-[#C8C9F787] lg:text-[16px] text-[12px]">
+      <input
+        autoComplete="off"
+        id="name"
+        placeholder=""
+        type="text"
+        className={`peer w-full p-4 pt-6 outline-none bg-transparent font-light border-b-2 border-[#C8C9F787] transition disabled:opacity-70 disabled:cursor-not-allowed 
+        `}
+      />
+      <label
+        htmlFor="name"
+        className={`absolute cursor-text duration-150 transform -translate-y-3 lg:top-4 top-5 z-10 origin-[0] left-4 peer-placeholder-shown:scale-[.9]
+        peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6 peer-focus:scale-[.6]`}
+      >
+        {title}
+      </label>
+    </div>
+  );
+};
+
+export default InputComponents;
