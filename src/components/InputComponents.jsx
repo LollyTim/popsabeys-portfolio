9172fd90620f6a3131db334be932465ea294aef5
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-03-20 16:25:59 +0100

    updated

--- src/components/InputComponents.jsx
+++ src/components/InputComponents.jsx
@@ -19,7 +19,6 @@ const InputComponents = ({ title }) => {
         {title}
         {/* askskaskasksakasksksa */}
         {/* askskaskasksakasksksa */}
-        {/* askskaskasksakasksksa */}
       </label>
     </div>
   );
